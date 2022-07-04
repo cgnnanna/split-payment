0c71561a2ad1d6907e9c93ad1cdf9e1290603fa5
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2022-07-04 02:00:02 +0100

    startup app using express and handle not found exceptions error

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const splitRoutes = require("./routes/splitRoutes");
+const app = express();
+
+app.use(express.json());
+app.use("/split-payments", splitRoutes);
+
+app.use((req, res) => {  
+    res.status(404).json({
+         message: "The route that you want to access does not exist"
+       });
+})
+
+let port = process.env.SERVER_PORT || 8000;
+app.listen(port, () => { console.log(`Server started on port ${port}`); })
+
+
