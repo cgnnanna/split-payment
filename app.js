ea7f8288970fd5b40595e631150142b6e97a9492
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2022-07-04 03:17:55 +0100

    removed morgan to take out the logging overhead

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const splitRoutes = require("./routes/splitRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.use("/split-payments", splitRoutes);
 
 app.use((req, res) => {  
